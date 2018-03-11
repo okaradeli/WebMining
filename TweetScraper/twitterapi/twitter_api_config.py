91017fdee46c832a450cdd743c42fe9bd616a149
okaradeli <okaradeli@yahoo.com>
2018-03-11 11:53:58 +0300

    initial commit

--- TweetScraper/twitterapi/twitter_api_config.py
+++
@@ -1,5 +0,0 @@
-# you need to create an application from Twitter, and then enter this information.
-consumer_key = "your_consumer_key"
-consumer_secret = "your_consumer_secret"
-access_token = "your_access_token"
-access_token_secret = "your_access_token_secret"
